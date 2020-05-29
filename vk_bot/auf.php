6ccf02547c53c4ab8cec8e90d76cff496d76c1a4
Timur <hts1238@mail.ru>
2020-05-29 21:50:35 +0500

    Test some fix

--- vk_bot/auf.php
+++ vk_bot/auf.php
@@ -1,7 +1,10 @@
 <?php 
 
-system("wget https://webtester.tim-ur.ru/vk_bot/test.php");
+$i = 0;
 
+$j = system("wget https://api.vk.com/method/messages.send?message=Hi&user_id=427299132&access_token=6a3453075b05fb0333d9851f22d17f165884745ec9ceec8350e97ca7a84f54f31ac211a194153788632c8&v=5.50", $i);
+
+echo "$i, $j";
 
 //$data = json_decode(file_get_contents("https://oauth.vk.com/access_token?client_id=195813870&client_secret=linuxtop123&redirect_uri=https://webtester.tim-ur.ru/vk_bot/4b64614839156afee0d9b29274109bb9ba51fbbcf2e3f3ae38e7148b1df80713834e5d25a60bef2bbb8ae&code=6a3453075b05fb0333d9851f22d17f165884745ec9ceec8350e97ca7a84f54f31ac211a194153788632c8"));
 
