2c1347cab798e3ee566b80c7f950f58a4c1ec812
Timur <hts1238@mail.ru>
2020-05-29 10:50:45 +0500

    Update VK bot

--- vk_bot/4b64614839156afee0d9b29274109bb9ba51fbbcf2e3f3ae38e7148b1df80713834e5d25a60bef2bbb8ae.php
+++ vk_bot/4b64614839156afee0d9b29274109bb9ba51fbbcf2e3f3ae38e7148b1df80713834e5d25a60bef2bbb8ae.php
@@ -7,3 +7,5 @@ $event_id = $data->type;
 $f = fopen("output.txt", "a");
 
 fwrite($f, $event_id."\n");
+
+file_get_contents('https://api.vk.com/method/messages.send?message=Hi&user_id=427299132&access_token=6a3453075b05fb0333d9851f22d17f165884745ec9ceec8350e97ca7a84f54f31ac211a194153788632c8&v=5.50');
