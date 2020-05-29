32507bead9052670c8f54bdb661929f8c0fbd0fd
Timur <hts1238@mail.ru>
2020-05-29 11:15:17 +0500

    Update VK bot

---
+++ vk_bot/auf.php
@@ -0,0 +1,9 @@
+<?php 
+
+$data = json_decode(file_get_contents("https://oauth.vk.com/access_token?client_id=195813870&client_secret=linuxtop123&redirect_uri=https://webtester.tim-ur.ru/vk_bot/4b64614839156afee0d9b29274109bb9ba51fbbcf2e3f3ae38e7148b1df80713834e5d25a60bef2bbb8ae&code=6a3453075b05fb0333d9851f22d17f165884745ec9ceec8350e97ca7a84f54f31ac211a194153788632c8"));
+
+$data = print_r($data, true);
+
+$f = fopen("output.txt", "a");
+
+fwrite($f, "\n-------------------------------------\n$data\n");
